1b6cb6b90986d66a9ade9ed04b290ff758c45a2a
Oliver Schulz <schulz.oliver93@googlemail.com>
2022-04-25 14:47:15 +0200

    Extended urlencoded limit

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.use(morgan("dev"));
 //app.use("/uploads", express.static("uploads"))
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json({ limit: "100mb" }));
+app.use(express.urlencoded({ limit: "5mb" }));
 app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
