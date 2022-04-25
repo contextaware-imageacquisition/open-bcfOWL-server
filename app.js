d80ca4a44c8397127a62edda13552462ebc8a5c6
Oliver Schulz <schulz.oliver93@googlemail.com>
2022-04-25 15:11:39 +0200

    Urlencoded

--- app.js
+++ app.js
@@ -27,9 +27,9 @@ const UserRouter = require("./api/userRoutes");
 
 app.use(morgan("dev"));
 //app.use("/uploads", express.static("uploads"))
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ limit: "5mb", extended: false }));
 app.use(express.json({ limit: "100mb" }));
-app.use(express.urlencoded({ limit: "5mb" }));
+//app.use(express.urlencoded({ limit: "5mb" }));
 app.use(express.json());
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
